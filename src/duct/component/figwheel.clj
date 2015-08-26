7ec94225939a5cf47c254ad8ced35ede305f8366
James Reeves <jreeves@weavejester.com>
2015-08-26 03:03:28 +0100

    Return new component from build function

--- src/duct/component/figwheel.clj
+++ src/duct/component/figwheel.clj
@@ -12,16 +12,15 @@
       (let [server  (fig-core/start-server component)
             builder (auto/make-conditional-builder (fig-auto/builder server))
             builds  (mapv auto/prep-build builds)]
-        (assoc component :server server, :builder builder, :prepped-builds builds))))
+        (assoc component :server server, :builder builder, :active-builds builds))))
   (stop [component]
     (if-let [server (:server component)]
       (do (fig-core/stop-server server)
-          (dissoc component :server :builder :prepped-builds))
+          (dissoc component :server :builder :active-builds))
       component)))
 
-(defn build [{:keys [builder prepped-builds]}]
-  (doseq [build prepped-builds]
-    (builder build)))
+(defn build [server]
+  (assoc server :active-builds (mapv (:builder server) (:active-builds server))))
 
 (defn server [options]
   (map->Server options))
