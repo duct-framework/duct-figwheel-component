3d48d00f103c6ba87b83cdb99ab26db8ab4c4cd2
James Reeves <jreeves@weavejester.com>
2015-08-26 23:12:28 +0100

    Hide huge figwheel data structures from print

--- src/duct/component/figwheel.clj
+++ src/duct/component/figwheel.clj
@@ -4,14 +4,29 @@
             [figwheel-sidecar.core :as fig-core]
             [figwheel-sidecar.auto-builder :as fig-auto]))
 
+(defrecord FigwheelBuild [])
+
+(defmethod clojure.core/print-method FigwheelBuild
+  [system ^java.io.Writer writer]
+  (.write writer "#<FigwheelBuild>"))
+
+(defrecord FigwheelServer [])
+
+(defmethod clojure.core/print-method FigwheelServer
+  [system ^java.io.Writer writer]
+  (.write writer "#<FigwheelServer>"))
+
+(defn- start-build [builder build]
+  (-> build auto/prep-build builder map->FigwheelBuild))
+
 (defrecord Server [builds]
   component/Lifecycle
   (start [component]
     (if (:server component)
       component
-      (let [server  (fig-core/start-server component)
+      (let [server  (map->FigwheelServer (fig-core/start-server component))
             builder (auto/make-conditional-builder (fig-auto/builder server))
-            state   (atom (mapv (comp builder auto/prep-build) builds))]
+            state   (atom (mapv (partial start-build builder) builds))]
         (assoc component :server server, :builder builder, :state state))))
   (stop [component]
     (if-let [server (:server component)]
@@ -20,7 +35,7 @@
       component)))
 
 (defn rebuild [{:keys [state builds builder]}]
-  (reset! state (mapv (comp builder auto/prep-build) builds)))
+  (reset! state (mapv (partial start-build builder) builds)))
 
 (defn build [{:keys [state builder]}]
   (swap! state (partial mapv builder)))
