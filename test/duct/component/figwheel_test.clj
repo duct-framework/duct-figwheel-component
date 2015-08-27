662509d32788b21a8a5757ae00665a9b1269d514
James Reeves <jreeves@weavejester.com>
2015-08-27 22:14:35 +0100

    Remove test directory

--- test/duct/component/figwheel_test.clj
+++
@@ -1,7 +0,0 @@
-(ns duct.component.figwheel-test
-  (:require [clojure.test :refer :all]
-            [duct.component.figwheel :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
